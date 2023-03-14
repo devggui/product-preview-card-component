8e9b3310be014258d6f9383d072838fd06ea69ba
devggui <devguiet@gmail.com>
2023-03-14 17:26:06 -0300

    feat: finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'cell': '374px',
+      }
+    },
   },
   plugins: [],
 }
